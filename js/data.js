4459faf3b6b4670824f6b95d3c6eeacba72cd762
Christina Le <christinaah02@gmail.com>
2022-02-15 17:08:29 -0800

    Verified that new entries are saved to local storage, verified that each new entry has incremeneting entryId, removed all css styling that was not needed, tested in privated browsing, and fixed all errors.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntries = localStorage.getItem('code-journal');
+
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var newEntryJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal', newEntryJSON);
+});
